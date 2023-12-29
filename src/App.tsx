4564b519d9f0596c97fb406d7d99f5f1e2f6c790
Emiliano Russo <russo.emiliano97@gmail.com>
2023-12-29 19:16:16 -0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="grid-container">
+      <div
+        className="grid-item"
+        style={{ gridColumnStart: "1", gridRowStart: "1", gridRowEnd: "3", background: "red" }}
+      >
+        <img src="https://i.pinimg.com/564x/e7/04/56/e7045643b35fba6754bf68e0dcae3bb0.jpg" />
+      </div>
+      <div className="grid-item" style={{ gridColumnStart: "2", gridColumnEnd: "4" }}>
+        <img src="https://images.pexels.com/photos/9754/mountains-clouds-forest-fog.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" />
+      </div>
+      <div className="grid-item" style={{ background: "cyan", gridColumnStart: "4" }}>
+        <h1>Hello 👋</h1>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Poulos Collective is a design consultancy that specializes in visual design and ux strategy for design
+          systems, mobile applications and desktop platforms.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <div className="grid-item" style={{ background: "gray", gridColumnStart: "2", gridRowStart: "2" }}></div>
+      <div
+        className="grid-item"
+        style={{ background: "green", gridColumnStart: "3", gridColumnEnd: "5", gridRowStart: "2", gridRowEnd: "4" }}
+      ></div>
     </div>
   );
 }
