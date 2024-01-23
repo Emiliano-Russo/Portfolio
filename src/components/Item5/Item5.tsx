7b0c2fc2310cd648454d568187e0f22e21b896bd
Emiliano Russo <russo.emiliano97@gmail.com>
2024-01-23 17:51:30 -0300

    Item 5 touches

--- src/components/Item5/Item5.tsx
+++ src/components/Item5/Item5.tsx
@@ -1,5 +1,6 @@
 import { useRef } from "react";
 import { ExperienceCard } from "../ExperienceCard/ExperienceCard";
+import "./Item5.scss";
 
 interface Props {
   ref: React.MutableRefObject<null>;
@@ -9,10 +10,10 @@ export const Item5 = (props: Props) => {
   return (
     <div ref={props.ref} id="item5" className="grid-item rightToLeft">
       <h2 className="work-experience-title">Work Experience</h2>
-      <ExperienceCard jobTitle="Freelancer" company="Upwork" period="2019-2020" />
-      <ExperienceCard jobTitle="Full Stack Developer" company="GlobalUY" period="2020-2023" />
-      <ExperienceCard jobTitle="Software Engineer III" company="Cencosud" period="2023-2024" />
-      <ExperienceCard jobTitle="Entrepreneur" company="Application Development" period="2024-Now" />
+      <ExperienceCard jobTitle="Freelancer" company="Upwork" period="2019 - 2020" />
+      <ExperienceCard jobTitle="Full Stack Developer" company="GlobalUY" period="2020 - 2023" />
+      <ExperienceCard jobTitle="Software Engineer III" company="Cencosud" period="2023 - 2024" />
+      <ExperienceCard jobTitle="Entrepreneur" company="App Development" period="2024 - Now" />
     </div>
   );
 };
