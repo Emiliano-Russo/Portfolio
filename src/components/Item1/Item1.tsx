1918884b067576abf671d5925542e77e1b06b8e8
Emiliano Russo <russo.emiliano97@gmail.com>
2024-03-11 22:42:58 -0300

    photo implemented

--- src/components/Item1/Item1.tsx
+++ src/components/Item1/Item1.tsx
@@ -1,9 +1,19 @@
-const myphoto = require("../../assets/me1.jpg");
+const myphoto = require("../../assets/me4.jpg");
 
 export const Item1 = () => {
   return (
     <div id="item1" className="grid-item leftToRight">
-      <img src={myphoto} />
+      <img
+        src={myphoto}
+        style={{
+          width: "190px",
+          height: "190px",
+          borderRadius: "50%",
+          objectFit: "cover",
+          objectPosition: "center 30%", // Esta es la posición predeterminada
+        }}
+        alt="Descripción de la imagen"
+      />
     </div>
   );
 };
